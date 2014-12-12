3153809426df489f4bc84c8c99acdd76847aaf20
Sebastian Gross <bigbasti@gmail.com>
2014-12-12 23:11:54 +0100

    Extended the model with Rewards

---
+++ F1Tippspiel.Db/Rewards/Achievement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using F1Tippspiel.Db.Account;
+
+namespace F1Tippspiel.Db.Rewards
+{
+    /// <summary>
+    /// Achievements are small prizes users can get for using the app.
+    /// like logging in 100 times.
+    /// </summary>
+    class Achievement
+    {
+        [Key]
+        public int AchievementId { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+
+        public virtual ICollection<UserAccount> UserAccounts { get; set; }
+    }
+}
---
+++ F1Tippspiel.Db/Rewards/Badge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using F1Tippspiel.Db.Account;
+
+namespace F1Tippspiel.Db.Rewards
+{
+    /// <summary>
+    /// Reprsents a badge which a user can earn for completing relatively complex tasks
+    /// like winning a season
+    /// </summary>
+    class Badge
+    {
+        [Key]
+        public int BadgeId { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+
+        public virtual ICollection<UserAccount> UserAccounts { get; set; }
+    }
+}
